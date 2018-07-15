058b37b045fca091b8c15a5e228f44ade9b52490
carolgreene <carolgreene3@gmail.com>
2018-07-15 14:30:40 -0700

    added sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  
   resources :applications
   resources :jobs
   resources :users
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
