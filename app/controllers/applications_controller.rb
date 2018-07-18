5bbeb2390e68b71a3b63adf98aa98e0dbb620892
carolgreene <carolgreene3@gmail.com>
2018-07-17 18:03:14 -0700

    initial applications controller & view code

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,2 +1,33 @@
 class ApplicationsController < ApplicationController
+
+  def new 
+    @application = Application.new 
+  end
+
+  def create 
+    @application = Application.create(application_params)
+    redirect_to application_path(@application)
+  end
+
+  def show 
+    @application = Application.find_by(id: params[:id])
+    @user = current_user 
+  end
+
+  def edit 
+    @application = Application.find_by(id: params[:id])
+    @user = current_user 
+  end 
+
+  def update 
+    @application = Application.find_by(id: params[:id])
+    @application.update(application_params)
+    redirect_to application_path(@application)
+  end
+
+  private 
+
+  def application_params 
+    params.require(:application).permit(:user_id, :job_id, :email, :qualifications)
+  end
 end
