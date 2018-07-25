1349f385aab17e63a36be00ae0f0b7cf0dd7eddb
carolgreene <carolgreene3@gmail.com>
2018-07-24 18:10:26 -0700

    added rolify gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,13 @@ class UsersController < ApplicationController
   end 
 
   def create 
-    @user = User.create(user_params)
+    
+    @user = User.new(user_params)
+    #if user_params[:admin] == "1"
+      #self.add_role(:admin) 
+    
+    #end
+
     session[:user_id] = @user.id 
     redirect_to user_path(@user)
   end 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  rolify
     has_many :applications
     has_many :jobs, through: :applications
     has_secure_password
