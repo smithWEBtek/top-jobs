57b93b02ab274f40f764dba9ec00b4a6ba4c26bf
carolgreene <carolgreene3@gmail.com>
2018-07-27 17:20:52 -0700

    added role to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
     has_many :applications
     has_many :jobs, through: :applications
     has_secure_password
+    enum role: [:user, :admin] 
+
+    def set_default_role 
+        self.role ||= :user 
+    end
 end
