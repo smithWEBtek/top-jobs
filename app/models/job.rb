bc6faa176de4a7adaedb944b4d22de06de353e21
carolgreene <carolgreene3@gmail.com>
2018-08-04 17:47:13 -0700

    a few changes

--- app/models/job.rb
+++ app/models/job.rb
@@ -4,5 +4,5 @@ class Job < ApplicationRecord
     belongs_to :company, :class_name => 'User', :foreign_key => 'company_id'
     has_many :applications 
     has_many :applicants, :through => :applications, :class_name => 'User' 
-
+    
 end
