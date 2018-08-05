703340c018969092f4d7b1a8ad8fd0281d4c7b71
carolgreene <carolgreene3@gmail.com>
2018-08-05 10:27:29 -0700

    fixed has_many_through associations

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,6 +3,6 @@ class Job < ApplicationRecord
     
     belongs_to :company, :class_name => 'User', :foreign_key => 'company_id'
     has_many :applications 
-    has_many :applicants, :through => :applications, :class_name => 'User' 
+    has_many :applicants, :through => :applications, :class_name => 'User', :foreign_key => 'applicant_id' 
     
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,18 @@
 class User < ApplicationRecord
     validates_presence_of :name, :password
     validates :name, uniqueness: true
-    
+    has_secure_password
 
     enum role: [:applicant, :company] 
 
     has_many :jobs, :foreign_key => 'company_id', source: :company
 
     has_many :applications, :foreign_key => 'user_id'
-    #has_many :jobs, through: :applications, :foreign_key => 'applicant_id', source: :applicant
-    has_secure_password
+    has_many :applied_jobs, through: :applications, :class_name => 'Job', :foreign_key => 'applicant_id', source: :applicant
+    
     
 
     def set_default_role 
-        self.role ||= :user 
+        self.role ||= :applicant
     end
 end
