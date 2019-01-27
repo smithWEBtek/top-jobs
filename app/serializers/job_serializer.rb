6baaeb53304817cf9a9d929b25c3195e36967c1b
carolgreene <carolgreene3@gmail.com>
2019-01-27 14:28:27 -0800

    added attributes & relationships for job_serializer

--- app/serializers/job_serializer.rb
+++ app/serializers/job_serializer.rb
@@ -1,3 +1,8 @@
 class JobSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :title, :location, :company_name, :company_id, :salary, :created_at, :category
+
+  belongs_to :company, :class_name => 'User', :foreign_key => 'company_id'
+  #has_many :job_applications 
+  #has_many :applicants, :through => :job_applications, :class_name => 'User', :foreign_key => 'applicant_id' 
+  #accepts_nested_attributes_for :job_applications
 end
