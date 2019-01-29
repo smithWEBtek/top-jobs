7bcdb95d52af3eee8f5dd20f266d921ab736f20d
carolgreene <carolgreene3@gmail.com>
2019-01-28 17:20:12 -0800

    Initial prototype for job index & create job objects

--- app/assets/javascripts/jobs.js
+++ app/assets/javascripts/jobs.js
@@ -5,12 +5,39 @@ $(document).ready(function() {
 const bindClickHandlers = () => {
  $('.all_jobs').on('click', (e) => {
      e.preventDefault()
+     history.pushState(null, null, "jobs")
      //alert('i was clicked')
      fetch(`/jobs.json`)
      .then((response) => response.json())
-     .then(data => {
+     .then(jobs => {
         $('#app-container').html('')
+        jobs.forEach(job => {    //or jobs.forEach(function(job) {
+            let newJob = new Job(job)
+            let jobHtml = newJob.formatIndex()
+            $(`#app-container`).append(jobHtml)
+            console.log(newJob)
+        })
      })
         
     })
+}
+
+function Job(job) {
+    this.id = job.id
+    this.title = job.title
+    this.salary = job.salary
+    this.category = job.category
+    this.company_name = job.company_name
+    this.company_id = job.company_id 
+    this.location = job.location
+    this.created_at = job.created_at
+}
+
+Job.prototype.formatIndex = function() {
+    let jobHtml = `
+    <a href="/jobs/${this.id}"><h3>${this.title}</h3></a> |
+    ${this.company_name} |
+    ${this.location} |
+    "Date Posted:"${this.created_at}`
+    return jobHtml
 }
